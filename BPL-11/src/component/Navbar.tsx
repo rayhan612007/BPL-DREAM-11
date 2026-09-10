026ce55c7416f902c2ec18ca4ebe6d6698765803
Rayhan_Sarker <mdaburayhan612007@gmail.com>
2026-09-10 20:59:12 +0600

    Remove unused import statement in Navbar.tsx

--- BPL-11/src/component/Navbar.tsx
+++ BPL-11/src/component/Navbar.tsx
@@ -1,9 +1,6 @@
-// import { SiBitcoinsv } from "react-icons/si";
 import React from "react";
-
 import logo from "../assets/logo.png";
 import { AiFillDollarCircle } from "react-icons/ai";
-
 
 export default function Navbar({coin} :{coin:number}) {
 
