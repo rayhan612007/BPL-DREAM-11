514f159269d62102d275eb1a3ee3094d9827e247
Rayhan_Sarker <mdaburayhan612007@gmail.com>
2026-09-10 20:54:15 +0600

    Remove unused import of useState in Navbar.tsx

--- BPL-11/src/component/Navbar.tsx
+++ BPL-11/src/component/Navbar.tsx
@@ -1,7 +1,7 @@
 // import { SiBitcoinsv } from "react-icons/si";
 import React from "react";
 
-import { useState } from "react";
+// import { useState } from "react";
 import logo from "../assets/logo.png";
 import { AiFillDollarCircle } from "react-icons/ai";
 
